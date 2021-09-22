f9fb3fa1ca4b1a0ca6fd9b202811321d2e5c15b3
Mouaz Saadaoui <mouaz@macbook-air-de-mouaz.home>
2021-09-22 15:30:53 +0200

    feat: complete last routes

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-
-Route::get('home', [StaticController::class, 'home']);
+Route::get('/', [StaticController::class, 'home']);
 
 Route::get('trips', [StaticController::class, 'trips']);
 
 Route::get('trip', [StaticController::class, 'trip']);
+
+Route::get('/aboutus', function () {
+    return view('aboutus.aboutus');
+});
+
+Route::get('/contact', function () {
+    return view('contact.contact');
+});
 
 Route::fallback(function() {
     return '404 : cette page n\'existe pas ou n\'existe plus.';
